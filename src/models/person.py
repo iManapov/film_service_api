fac80861e07dd2fa8c6c8556faa2305e324385fd
iManapov <manapovilnar@gmail.com>
2022-10-03 00:26:44 +0300

    Added persons endpoint

--- src/models/person.py
+++ src/models/person.py
@@ -1,10 +1,24 @@
 import uuid
 
 from typing import Optional
+from pydantic import BaseModel
 
 from models.mixin import JsonMixin
 
 
-class Persons(JsonMixin):
+class Person(JsonMixin):
+    """Модель персоналий в ES."""
+
     id: uuid.UUID
     name: str
+    role: Optional[list[str]]
+    film_ids: Optional[list[uuid.UUID]]
+
+
+class BasePersonApi(BaseModel):
+    """API-Модель для описания персоналий."""
+
+    uuid: uuid.UUID
+    name: str
+    role: Optional[list[str]]
+    film_ids: Optional[list[uuid.UUID]]
---
+++ src/services/person.py
@@ -0,0 +1,134 @@
+import uuid
+
+from typing import Optional, Union, Tuple
+
+from aioredis import Redis
+from elasticsearch import AsyncElasticsearch, NotFoundError
+from elasticsearch.exceptions import RequestError
+from fastapi import Depends, Request
+
+from db.elastic import get_elastic
+from db.redis import get_redis
+from models.person import Person
+from models.film import BaseFilmApi
+from utils.cache import Cache
+from utils.sort_string import clear_sort_string
+
+
+class PersonService:
+    def __init__(self, request: Request, redis: Redis, elastic: AsyncElasticsearch):
+        self.redis = redis
+        self.elastic = elastic
+        self.cache = Cache(request, redis)
+
+    async def get_person_by_id(self, person_id: uuid.UUID) -> Optional[Person]:
+        """
+        Получаем персону по uuid
+
+        @param person_id: uuid персоны
+        @return: объект-персона
+        """
+        person = await self.cache.get()
+
+        if not person:
+            try:
+                person = await self.elastic.get('persons', person_id)
+            except NotFoundError:
+                return None
+            await self.cache.set(person)
+
+        person = Person(**person['_source'])
+        return person
+
+    async def get_persons(
+            self,
+            sort: Optional[str] = None,
+            limit: Optional[int] = 50,
+            page: Optional[int] = 1,
+            query: Optional[str] = None
+    ) -> Tuple[Union[list[Person], None], list[str]]:
+        """
+        Получает список персоналий по запросу query
+
+        @param sort: имя поля по которому идет сортировка
+        @param limit: количество записей на странице
+        @param page: номер страницы
+        @param query: поисковый запрос
+        @return: Данные по персоналиям
+        """
+
+        errors = []
+        query_ = None
+
+        persons = await self.cache.get()
+
+        if not persons:
+            sort = clear_sort_string(sort, 'name')
+
+            if query:
+                query_ = {
+                    "query": {
+                        "query_string": {
+                            "default_field": "name",
+                            "query": query
+                        }
+                    }
+                }
+
+            try:
+                persons = await self.elastic.search(
+                    index='persons',
+                    body=query_,
+                    size=limit,
+                    sort=sort,
+                    from_=limit * (page - 1)
+                )
+                persons = persons['hits']['hits']
+                await self.cache.set(persons)
+
+            except RequestError:
+                persons = None
+                errors.append('В запрашиваемых параметрах содержатся ошибки')
+
+        return persons, errors
+
+    async def get_persons_film(self, person_id: uuid.UUID) \
+            -> Tuple[Union[list[BaseFilmApi], None], list[str]]:
+        """
+        Метод получения фильмов с участием персоналии с person_id.
+
+        @param person_id: id персоналии
+        @return: список фильмов персоналии
+        """
+
+        errors = []
+
+        films = await self.cache.get()
+
+        if not films:
+            films = []
+            person = await self.get_person_by_id(person_id)
+            if not person:
+                errors.append('Wrong user uuid')
+                return None, errors
+            for film_id in person.film_ids:
+                try:
+                    film = await self.elastic.get('movies', film_id)
+                    films.append(film)
+                except NotFoundError:
+                    pass
+        await self.cache.set(films)
+
+        return films, errors
+
+
+def get_person_service(
+        request: Request,
+        redis: Redis = Depends(get_redis),
+        elastic: AsyncElasticsearch = Depends(get_elastic),
+) -> PersonService:
+    """
+    Провайдер PersonService,
+    с помощью Depends он сообщает, что ему необходимы Redis и Elasticsearch
+    """
+    return PersonService(request, redis, elastic)
